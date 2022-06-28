a70d2b2a54e044be7310893cbee4c6fccf0c9048
Nadezhda <nbykova79@gmail.com>
2022-06-28 22:03:03 +0300

    исправили при варианте, что числа равны

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// определить макс и мин из двух чисел
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.Write("числа равны");
+}
+
+if (num1 > num2) 
+{
+    Console.Write("максимальное число ");
+    Console.WriteLine(num1);
+    Console.Write("минимальное число ");
+    Console.WriteLine(num2);
+}
+if (num1 < num2) 
+{
+    Console.Write("максимальное число ");
+    Console.WriteLine(num1);
+    Console.Write("минимальное число ");
+    Console.WriteLine(num2);
+}
